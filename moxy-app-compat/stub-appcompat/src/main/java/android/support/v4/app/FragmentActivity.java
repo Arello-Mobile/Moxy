4df0bfeb9cf2d56ac40f33a556016ff85ccfd5ca
senneco <senneco@gmail.com>
2016-10-12 01:26:10 +0700

    Fixed NoSuchMethodError when call MvpAppCompatFragment.onDestroy()

---
+++ moxy-app-compat/stub-appcompat/src/main/java/android/support/v4/app/FragmentActivity.java
@@ -0,0 +1,11 @@
+package android.support.v4.app;
+
+/**
+ * Created by senneco on 11.10.2016
+ */
+
+public class FragmentActivity {
+	public boolean isFinishing() {
+		throw new RuntimeException("Stub!");
+	}
+}
