8f4a432ca68d1c7d927508b46f5dd1e70d027fec
Yuri Shmakov <shmakov@arello-mobile.com>
2016-12-29 19:03:46 +0700

    [feature] Test memory leaks

---
+++ moxy/src/test/java/com/arellomobile/mvp/memory_leak_test/resources/TestPresenter.java
@@ -0,0 +1,12 @@
+package com.arellomobile.mvp.memory_leak_test.resources;
+
+import com.arellomobile.mvp.MvpPresenter;
+
+/**
+ * Date: 29.12.2016
+ * Time: 14:32
+ *
+ * @author Yuri Shmakov
+ */
+public class TestPresenter extends MvpPresenter<TestView> {
+}
