eafed8097830b0aac8c7e81dd335a4544d215a88
vova <v.stelmashchuk@mobidev.biz>
2018-07-25 02:09:13 +0300

    add moxy android x

---
+++ moxy-androidx-sapmle/src/main/java/example/com/moxy_androidx_sapmle/MainActivity.kt
@@ -0,0 +1,12 @@
+package example.com.moxy_androidx_sapmle
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
