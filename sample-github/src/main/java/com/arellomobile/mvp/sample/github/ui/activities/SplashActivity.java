2e08053f703df2e6189057a6dce6008c553bc461
vova <v.stelmashchuk@mobidev.biz>
2018-08-29 15:59:01 +0300

    fixed git hub sampble

--- sample-github/src/main/java/com/arellomobile/mvp/sample/github/ui/activities/SplashActivity.java
+++ sample-github/src/main/java/com/arellomobile/mvp/sample/github/ui/activities/SplashActivity.java
@@ -10,20 +10,24 @@ import com.arellomobile.mvp.sample.github.mvp.views.SplashView;
 
 public class SplashActivity extends MvpAppCompatActivity implements SplashView {
 
-	@InjectPresenter
-	SplashPresenter mSplashPresenter;
+    @InjectPresenter
+    SplashPresenter mSplashPresenter;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		// By default view attaches to presenter in onResume() method,
-		// but we attach it manually for earlier check authorization state.
-		getMvpDelegate().onAttach();
-	}
+        // By default view attaches to presenter in onResume() method,
+        // but we attach it manually for earlier check authorization state.
+        getMvpDelegate().onAttach();
+    }
 
-	@Override
-	public void setAuthorized(boolean isAuthorized) {
-		startActivity(new Intent(this, isAuthorized ? HomeActivity.class : SignInActivity.class));
-	}
+    @Override
+    public void setAuthorized(boolean isAuthorized) {
+        if (isAuthorized) {
+            startActivity(new Intent(this, HomeActivity.class));
+        } else {
+            startActivity(new Intent(this, SignInActivity.class));
+        }
+    }
 }
