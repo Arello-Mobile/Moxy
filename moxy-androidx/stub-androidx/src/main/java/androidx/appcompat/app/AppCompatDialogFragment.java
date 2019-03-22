5cfafbe12ddd343d4f677caae82a4a4a77d2a3ff
roman_tcaregorodtcev <roman.tc@omega-r.com>
2019-03-22 09:19:44 +0300

    MvpAppCompatDialogFragment has been added

---
+++ moxy-androidx/stub-androidx/src/main/java/androidx/appcompat/app/AppCompatDialogFragment.java
@@ -0,0 +1,13 @@
+package androidx.appcompat.app;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Date: 22-March-19
+ * Time: 09:18
+ *
+ * @author R12rus
+ */
+public class AppCompatDialogFragment extends Fragment {
+
+}
